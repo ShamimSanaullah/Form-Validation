cc8b8dd2b83ed19a714b6dc40f849d8e3969c40d
Shamim Sanaullah <shamimsanaullah13@gmail>
2024-01-06 19:01:48 +0530

    css added

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const cpassword = document.getElementById("cpassword");
+
+form.addEventListener('submit',(event) =>{
+    event.preventDefault();
+})
+
